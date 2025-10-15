ffadf62a53fdf86be0818bac6f777d9e9f1df1e8
Agrim <agrimdubey37@gmail.com>
2025-10-15 18:59:25 +0530

    Changefd URls to match design and created a model for doctors

--- Authapi/models.py
+++ Authapi/models.py
@@ -1,16 +1,54 @@
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin , 
 from django.db import models
+from accounts.from django.db import models import CustomUser
+from django.utils import timezone
+from django.core.validators import MinValueValidator
+from django.core.validators import RegexValidator
 
-class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('doctor', 'Doctor'),
-        ('patient', 'Patient'),
-    )
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    otp = models.CharField(max_length=6, blank = True, null = True)
-    otp_created_at = models.DateTimeField(blank=True, null=True)
-    is_verified = models.BooleanField(default=False)
+
+class Doctors(models.Model):
+    gender_choice = (('male','Male'),
+                   ('female','Female'),
+                   ('Prefer not to say','Prefer not to say')
+                   ),
+    phone_validator = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    user = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    date_of_birth = models.DateField(help_text="DD-MM-YYYY",verbose_name="Date",blank=False)
+    gender = models.CharField(max_length=20,choices = gender_choice,blank=False)
+    specialization = models.CharField(max_length=100,blank=False)
+    department = models.CharField(max_length= 100,blank=False)
+    experience = models.IntegerField(validators=[MinValueValidator(0)],blank=False)
+    clinicaladdress = models.CharField(max_length = 100)
+    phone_number = models.CharField(
+    max_length=15,
+    validators=[phone_validator],
+    blank=False
+)
+
+    license_number = models.CharField(max_length=30, blank=False)
+    medical_degree = models.FileField(upload_to='degrees/',blank=False)
+    license_certificate = models.FileField(upload_to='licenses/',blank=False)
+    university = models.CharField(max_length=60,blank=False)
 
     def __str__(self):
-        return f"{self.username} ({self.role})"
+        return f"{self.user.username} - {self.specialization}"
+
+
+
+
+
+
+##################################################################################################################
+# class CustomUser(AbstractUser):
+#     ROLE_CHOICES = (
+#         ('doctor', 'Doctor'),
+#         ('patient', 'Patient'),
+#     )
+#     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+#     otp = models.CharField(max_length=6, blank = True, null = True)
+#     otp_created_at = models.DateTimeField(blank=True, null=True)
+#     is_verified = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return f"{self.username} ({self.role})"
--- Authapi/urls.py
+++ Authapi/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/register/', views.register_view, name='register'),
-    path('api/verify-otp/', views.verify_otp_view, name='verify_otp'),
-    path('api/login/', views.login_view, name='login'),
+    path('api/docregister/', views.doctor_register_view, name='doctor_register'),
+    path('api/doclogin/', views.patient_login_view, name='doctor_login'),
+    path('api/patientregister/',views.patient_register_view,name ='patient_register'),
+    path('api/patientlogin/',views.patient_login_view,name='patient_login'),
     path('api/forgot-password/', views.forgot_password_view, name='forgot_password'),
+    path('api/verify-otp/', views.verify_otp_view, name='verify_otp'),
     path('api/reset-password/', views.reset_password_view, name='reset_password'),
     path('api/resend-otp/', views.resend_otp_view, name='resend_otp'),
-    path('api/logout/', views.logout_view, name='logout'),
+    path('api/Doctor-Details/',views.doctor_details_view,name='doctor_details_page'),
+    path('api/Patient-Details/',views.patients_details_view,name='patient_details_page'),
 ]
