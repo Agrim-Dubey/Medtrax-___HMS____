156b110244f6ce2728647e112de798cffdeab4d9
Agrim <agrimdubey37@gmail.com>
2025-10-15 08:52:43 +0530

    Switched to postgresql database

--- medtrax/settings.py
+++ medtrax/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'medtrax.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Medtraxdatabase',
+        'USER': 'Myuser',
+        'PASSWORD': 'Medtrax2025',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 AUTH_USER_MODEL = 'Authapi.CustomUser'
