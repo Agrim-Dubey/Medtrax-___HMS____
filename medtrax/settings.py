774b03705c0d3f83ee0e430b3a92578c72785e22
Agrim <agrimdubey37@gmail.com>
2025-10-28 18:35:43 +0530

    .

--- medtrax/settings.py
+++ medtrax/settings.py
@@ -138,9 +138,26 @@ CORS_ALLOWED_ORIGINS = [
     'https://www.medtrax.me',
 ]
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = ['*']
-CORS_ALLOW_METHODS = ['*']
-CORS_EXPOSE_HEADERS = ['*']
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 CORS_PREFLIGHT_MAX_AGE = 86400
 
 CSRF_COOKIE_SECURE = False
