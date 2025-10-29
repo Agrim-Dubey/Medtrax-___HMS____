d5a51bdb90726ad5509bbe636159fe021eee942d
Agrim <agrimdubey37@gmail.com>
2025-10-29 10:47:28 +0530

    Change in session settings

--- medtrax/asgi.py
+++ medtrax/asgi.py
@@ -1,13 +1,18 @@
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medtrax.settings')
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat_room.middleware import JWTAuthMiddleware
 from chat_room.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medtrax.settings')
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': JWTAuthMiddleware(
         URLRouter(
             websocket_urlpatterns
