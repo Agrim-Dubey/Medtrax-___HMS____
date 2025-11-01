f8e5747217d190aa1746ebb421263b693e88b161
Agrim <agrimdubey37@gmail.com>
2025-11-01 05:51:15 +0530

    Added second api endpoint and its view

--- patient_dashboard/serializers.py
+++ patient_dashboard/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from Authapi.models  import Patient
+from appointments.models import Appointment
 
-class PatientDashboardserializer(serializers.ModelSerializer):
+class PatientDashboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = [
@@ -12,4 +13,20 @@ class PatientDashboardserializer(serializers.ModelSerializer):
             'known_allergies',
             'chronic_diseases'
         ]
+
+class DashboardAppointmentSerializer(serializers.ModelSerialzier):
+    doctor_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Appointment
+        fields = [
+            'id',
+            'doctor_name',
+            'appointment_date',
+            'appointment_time',
+            'reason',
+            'status'
+        ]
+    def get_doctor_name(self, obj):
+        return f"Dr. {obj.doctor.get_full_name()}"
 
--- patient_dashboard/urls.py
+++ patient_dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PatientDashboardView
+from .views import PatientDashboardView, PatientUpcomingAppointmentsView
 
 urlpatterns = [
     path('profile/', PatientDashboardView.as_view(), name='patient-dashboard-profile'),
+    path('appointments/', PatientUpcomingAppointmentsView.as_view(), name='patient-upcoming-appointments'),
 ]
