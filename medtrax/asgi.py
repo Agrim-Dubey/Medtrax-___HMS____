f4a78830cba4e5a4068c113bc686cbfdcd25fce6
Agrim <agrimdubey37@gmail.com>
2025-11-02 13:26:36 +0530

    Changed asgi.py import order to prevent daphne from crashing

--- medtrax/asgi.py
+++ medtrax/asgi.py
@@ -9,8 +9,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medtrax.settings')
 
 from urllib.parse import parse_qs
 from django.contrib.auth.models import AnonymousUser
-from rest_framework_simplejwt.tokens import AccessToken
-from Authapi.models import CustomUser
 from channels.db import database_sync_to_async
 
 class JWTAuthMiddleware:
@@ -18,24 +16,27 @@ class JWTAuthMiddleware:
         self.app = app
 
     async def __call__(self, scope, receive, send):
+        from rest_framework_simplejwt.tokens import AccessToken   
+        from Authapi.models import CustomUser                      
+
         query_string = scope.get('query_string', b'').decode()
         params = parse_qs(query_string)
         token = params.get('token', [None])[0]
-        
+
         if token:
             try:
                 access_token = AccessToken(token)
-                user = await self.get_user(access_token['user_id'])
+                user = await self.get_user(CustomUser, access_token['user_id'])
                 scope['user'] = user
             except Exception:
                 scope['user'] = AnonymousUser()
         else:
             scope['user'] = AnonymousUser()
-        
+
         return await self.app(scope, receive, send)
-    
+
     @database_sync_to_async
-    def get_user(self, user_id):
+    def get_user(self, CustomUser, user_id):
         try:
             return CustomUser.objects.get(id=user_id)
         except CustomUser.DoesNotExist:
