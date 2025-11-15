9fdd7c958994137c6fabea2fbc5aede1691fb725
Agrim <agrimdubey37@gmail.com>
2025-11-15 06:04:25 +0530

    Configured s3 bucket for image storing

--- medtrax/settings.py
+++ medtrax/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'Authapi',
     'chat_room',
     'videocounselling',
+    'storages',
     'appointments',
     'doctor_dashboard',
     'community',
@@ -100,6 +101,22 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = "eu-north-1"
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+AWS_QUERYSTRING_AUTH = True
+AWS_S3_VERIFY = True
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 
 AUTH_USER_MODEL = 'Authapi.CustomUser'
 AUTH_PASSWORD_VALIDATORS = []
